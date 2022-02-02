8abb55c32a022ee3254e5befe57056b527ccb47f
Alexander Velarde <alexandervelarde2511@gmail.com>
2022-02-02 11:45:56 -0400

    configurando la parte inicial del layouts=plantiya y la vista  index

---
+++ j-livewire-8/resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.web')
+
+@section('content')
+    <div class="text-center">
+        <h1 class="text-2xl text-gray-700 mb-4 uppercase">Útimos cursos</h1>
+        <h2 class="text-xl text-gray-600">Fórmate como profesional en la tecnología</h2>
+        <h3 class="text-lg text-gray-600">70% de losgraduados duplica sus ingresos</h3>
+    </div>
+@endsection
