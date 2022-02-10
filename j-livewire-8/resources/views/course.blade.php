d8335e9d2f5d29536640f439942838b8f10f0a0a
Alexander Velarde <alexandervelarde2511@gmail.com>
2022-02-10 19:13:10 -0400

    configuramos rutas y controladores para cada curso sea  mas personalizado

---
+++ j-livewire-8/resources/views/course.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.web')
+
+@section('content')
+    detalle del  curso
+    <div class="text-center">
+        <h1 class="text-2xl text-gray-700 mb-4 uppercase">Útimos cursos</h1>
+        <h2 class="text-xl text-gray-600">Fórmate como profesional en la tecnología</h2>
+    </div>
+    <livewire:course-list>
+@endsection
